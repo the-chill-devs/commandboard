1c20f63fa238ae144079d4bf93a3b29a5751e454
codeschris <chrismlolwa42@outlook.com>
2025-04-21 17:58:22 +0300

    feat: initialize Tauri for Desktop Application

--- cbfront/src/App.tsx
+++ cbfront/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           alt="logo"
         />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload with Chris.
         </p>
         <a
           className="text-[#61dafb] hover:underline"
--- cbfront/vite.config.js
+++ cbfront/vite.config.js
@@ -6,6 +6,10 @@ import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 1420,          // Tauri default
+    strictPort: true,    // avoids port conflicts
+  },
   plugins: [viteReact(), tailwindcss()],
   test: {
     globals: true,
